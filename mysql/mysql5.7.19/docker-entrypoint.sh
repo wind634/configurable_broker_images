6ff4e6d0bfd8d69d2b6697ab9ca88aa70e581cfd
wind634 <2911184332@qq.com>
2017-09-04 22:05:14 +0800

    mysql增加对配置文件的支持

--- mysql/mysql5.7.19/docker-entrypoint.sh
+++ mysql/mysql5.7.19/docker-entrypoint.sh
@@ -6,6 +6,8 @@
 # MYSQL_WAIT_TIMEOUT    服务器关闭非交互连接之前等待活动秒数
 # 在/etc/mysql/conf.d目录下新建配置文件
 
+# MYSQL_CONF_FILE_*
+
 set -eo pipefail
 shopt -s nullglob
 
@@ -75,55 +77,112 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" -a "$(id -u)" = '0' ]; then
 	chown -R mysql:mysql "$DATADIR"
 
      #  add env config start
+    USE_MYSQL_CONF_FILE="false"
+    mysql_conf_file_array=()
+
+    for LINE in `env`
+    do
+        # 逐行读取  等号分割
+        env_key=`echo $LINE | cut -d = -f 1`
+        case $env_key in
+            "MYSQL_CONF_FILE_"*)
+                # 只要该环节变量存在,说明是使用配置文件的形式
+                USE_MYSQL_CONF_FILE="true"
+                mysql_conf_file_array+=($env_key)
+            ;;
+            *)
+                :
+            ;;
+        esac
+    done
+
+
     CONFIG_DIR="/etc/mysql/conf.d"
     if [ ! -f "$CONFIG_DIR/new_config.cnf" ]; then
         CONFIG="$CONFIG_DIR/new_config.cnf"
         echo  "[mysqld]" >> "$CONFIG"
 
-        file_env 'MYSQL_MAX_CONNECTIONS'
-        if [ -n "$MYSQL_MAX_CONNECTIONS" ];then
-            if grep '^[[:digit:]]*$' <<< "$MYSQL_MAX_CONNECTIONS";then
-                if [ $MYSQL_MAX_CONNECTIONS -gt 0 ]; then
-                    echo  "max_connections=$MYSQL_MAX_CONNECTIONS" >> "$CONFIG"
+        if [ $USE_MYSQL_CONF_FILE == "true" ];then
+            # 以配置文件的方式写入
+            # 读取每一行环境变量
+            # 按照末尾序号进行排序
+            len=${#mysql_conf_file_array[@]}
+            for((i=0; i<$len; i++)){
+              for((j=i+1; j<$len; j++)){
+                 num1=${mysql_conf_file_array[i]:16}
+                 num2=${mysql_conf_file_array[j]:16}
+                 echo "num1:"$num1
+                 echo "num2:"$num2
+                if [[ ${num1} -gt ${num2} ]]
+                then
+                  temp=${mysql_conf_file_array[i]}
+                  mysql_conf_file_array[i]=${mysql_conf_file_array[j]}
+                  mysql_conf_file_array[j]=$temp
                 fi
-            else
-                echo "max_connections value is not a number."
-            fi
-        fi
+              }
+            }
 
-        file_env 'MYSQL_QUERY_CACHE_SIZE'
-        if [ -n "$MYSQL_QUERY_CACHE_SIZE" ];then
-            if grep '^[[:digit:]]*$' <<< "$MYSQL_QUERY_CACHE_SIZE";then
-                if [ $MYSQL_QUERY_CACHE_SIZE -gt 0 ]; then
-                    echo  "query_cache_size=${MYSQL_QUERY_CACHE_SIZE}kb" >> "$CONFIG"
+            # 重定向到配置文件路径
+            for ((i=0;i<${#mysql_conf_file_array[@]};i++))
+            do
+                arg=${mysql_conf_file_array[$i]}
+                var="$arg"
+                val=${!var}
+                # 每一行进行正则过滤，不符合格式则不写入
+                checkedVal=`echo "$val" | grep -E '^(\s*#\s*(\S*\s*)*\s*)|(\s*\w+\s*=\s*\S+\s*)$' `
+
+                if [ -n "$checkedVal" ]; then
+                    echo "env_file_row ${arg}'s content..."${val}
+                    echo "$val" >> "$CONFIG"
                 fi
-            else
-                echo "query_cache_size value is not a number."
-            fi
-        fi
+            done
 
-        file_env 'MYSQL_CONNECT_TIMEOUT'
-        if [ -n "$MYSQL_CONNECT_TIMEOUT" ];then
-            if grep '^[[:digit:]]*$' <<< "$MYSQL_CONNECT_TIMEOUT";then
-                if [ $MYSQL_CONNECT_TIMEOUT -ge 0 ]; then
-                    echo  "connect_timeout=$MYSQL_CONNECT_TIMEOUT" >> "$CONFIG"
+        else
+            file_env 'MYSQL_MAX_CONNECTIONS'
+            if [ -n "$MYSQL_MAX_CONNECTIONS" ];then
+                if grep '^[[:digit:]]*$' <<< "$MYSQL_MAX_CONNECTIONS";then
+                    if [ $MYSQL_MAX_CONNECTIONS -gt 0 ]; then
+                        echo  "max_connections=$MYSQL_MAX_CONNECTIONS" >> "$CONFIG"
+                    fi
+                else
+                    echo "max_connections value is not a number."
                 fi
-            else
-                echo "connect_timeout value is not a number."
             fi
-        fi
 
-        file_env 'MYSQL_WAIT_TIMEOUT'
-        if [ -n "$MYSQL_WAIT_TIMEOUT" ];then
-            if grep '^[[:digit:]]*$' <<< "$MYSQL_WAIT_TIMEOUT";then
-                if [ $MYSQL_WAIT_TIMEOUT -gt 0 ]; then
-                    echo  "wait_timeout=$MYSQL_WAIT_TIMEOUT" >> "$CONFIG"
+            file_env 'MYSQL_QUERY_CACHE_SIZE'
+            if [ -n "$MYSQL_QUERY_CACHE_SIZE" ];then
+                if grep '^[[:digit:]]*$' <<< "$MYSQL_QUERY_CACHE_SIZE";then
+                    if [ $MYSQL_QUERY_CACHE_SIZE -gt 0 ]; then
+                        echo  "query_cache_size=${MYSQL_QUERY_CACHE_SIZE}kb" >> "$CONFIG"
+                    fi
+                else
+                    echo "query_cache_size value is not a number."
                 fi
-            else
-                echo "wait_timeout value is not a number."
             fi
-        fi
 
+            file_env 'MYSQL_CONNECT_TIMEOUT'
+            if [ -n "$MYSQL_CONNECT_TIMEOUT" ];then
+                if grep '^[[:digit:]]*$' <<< "$MYSQL_CONNECT_TIMEOUT";then
+                    if [ $MYSQL_CONNECT_TIMEOUT -ge 0 ]; then
+                        echo  "connect_timeout=$MYSQL_CONNECT_TIMEOUT" >> "$CONFIG"
+                    fi
+                else
+                    echo "connect_timeout value is not a number."
+                fi
+            fi
+
+            file_env 'MYSQL_WAIT_TIMEOUT'
+            if [ -n "$MYSQL_WAIT_TIMEOUT" ];then
+                if grep '^[[:digit:]]*$' <<< "$MYSQL_WAIT_TIMEOUT";then
+                    if [ $MYSQL_WAIT_TIMEOUT -gt 0 ]; then
+                        echo  "wait_timeout=$MYSQL_WAIT_TIMEOUT" >> "$CONFIG"
+                    fi
+                else
+                    echo "wait_timeout value is not a number."
+                fi
+            fi
+
+        fi
     fi
     #  add env config end
 
