fffc146e76f09ab6ff48a0f1644689dc0d11d32b
wind634 <2911184332@qq.com>
2017-09-05 17:01:48 +0800

    java8镜像

---
+++ java/jdk1.8/docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#JVM_XMS
+#JVM_XMX
+#JVM_XSS
+
+java_opts=""
+if [ -n "$JVM_XMS" ]; then
+    java_opts+="-Xmx"$JVM_XMS"k"
+fi
+if [ -n "$JVM_XMX" ]; then
+    java_opts+="-Xmx"$JVM_XMX"k"
+fi
+if [ -n "$JVM_XSS" ]; then
+    java_opts+="-Xmx"$JVM_XSS"k"
+fi
+
+echo java_opts
+if [ -n "$java_opts" ]; then
+    echo "aaa"
+    echo "$1"
+    exec "$1"
+else
+    exec "$@"
+fi
+
+
