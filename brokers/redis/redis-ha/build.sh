5539aa47f595a8f3a09f94285071944890ca40ab
wind634 <2911184332@qq.com>
2017-09-06 16:25:18 +0800

    完善redis-sentinel

---
+++ brokers/redis/redis-ha/build.sh
@@ -0,0 +1,13 @@
+cd .
+cd ./redis-node/
+docker build -t wind634/redis .
+cd ..
+cd ./redis-sentinel/
+docker build -t wind634/redis-sentinel  .
+cd ..
+cd ./haproxy/
+docker build -t wind634/haproxy-redis  .
+
+#docker push wind634/haproxy-redis
+#docker push wind634/redis
+#docker push wind634/redis-sentinel
