2670149f9b94b8ff0f5f24af9e3998797015b0ae
wind634 <2911184332@qq.com>
2017-09-06 23:28:51 +0800

    测试脚本

--- brokers/redis/redis-ha/haproxy/docker-entrypoint.sh
+++ brokers/redis/redis-ha/haproxy/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "s/{{balance}}/$HAPROXY_BALANCE/g" /usr/local/etc/haproxy/haproxy.cfg
-sed -i "s/auth_key/$REDIS_AUTH_PASSWORD/g" /usr/local/etc/haproxy/haproxy.cfg
+sed -i "s/{{balance}}/${HAPROXY_BALANCE}/g" /usr/local/etc/haproxy/haproxy.cfg
+sed -i "s/auth_key/${REDIS_AUTH_PASSWORD}/g" /usr/local/etc/haproxy/haproxy.cfg
 sed -i "s/server master master:6379 check inter 1s/server ${APPNAME}_master ${APPNAME}_master:6379 check inter 1s/g" /usr/local/etc/haproxy/haproxy.cfg
 sed -i "s/server slave slave:6379 check inter 1s/server ${APPNAME}_slave ${APPNAME}_slave:6379 check inter 1s/g" /usr/local/etc/haproxy/haproxy.cfg
 sed -i "s/server slave1 slave1:6379 check inter 1s/server ${APPNAME}_slave1 ${APPNAME}_slave1:6379 check inter 1s/g" /usr/local/etc/haproxy/haproxy.cfg
