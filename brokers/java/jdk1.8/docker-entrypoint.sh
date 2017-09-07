a07f9380ec644318a60a46b314bdf3805d18b262
wind634 <2911184332@qq.com>
2017-09-07 21:33:09 +0800

    mysql镜像

--- brokers/java/jdk1.8/docker-entrypoint.sh
+++ brokers/java/jdk1.8/docker-entrypoint.sh
@@ -5,20 +5,45 @@
 
 java_opts=""
 if [ -n "$JVM_XMS" ]; then
-    java_opts+="-Xmx"$JVM_XMS"k"
+    if grep '^[[:digit:]]*$' <<< "$JVM_XMS";then
+       if [ $JVM_XMS -gt 0 ]; then
+           java_opts+="-Xmx"$JVM_XMS"k"
+       fi
+    fi
 fi
 if [ -n "$JVM_XMX" ]; then
-    java_opts+="-Xmx"$JVM_XMX"k"
+    if grep '^[[:digit:]]*$' <<< "$JVM_XMX";then
+       if [ $JVM_XMX -gt 0 ]; then
+            java_opts+="-Xmx"$JVM_XMX"k"
+       fi
+   fi
 fi
 if [ -n "$JVM_XSS" ]; then
-    java_opts+="-Xmx"$JVM_XSS"k"
+   if grep '^[[:digit:]]*$' <<< "$JVM_XSS";then
+       if [ $JVM_XSS -gt 0 ]; then
+            java_opts+="-Xmx"$JVM_XSS"k"
+       fi
+   fi
 fi
 
-echo #java_opts
+#echo java_opts
 if [ -n "$java_opts" ]; then
-    exec "$1" $java_opts "$2"
+    if [ $1 == "java" ];then
+        # 获取2~n个参数
+        output=""
+        for((i=2;i<=$#;i++)); do
+            j=${!i}
+            output="${output} $j "
+        done
+        echo ${output}
+        exec `$1 $java_opts $output`
+	else
+        exec "$@"
+	fi
 else
     exec "$@"
 fi
+
+
 
 
