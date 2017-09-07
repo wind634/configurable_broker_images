292a9d472b004d5015287415045302bea00f949c
wind634 <2911184332@qq.com>
2017-09-07 15:54:33 +0800

    redis镜像调整

--- brokers/redis/redis-ha/haproxy/docker-entrypoint.sh
+++ brokers/redis/redis-ha/haproxy/docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+
+sed -i "s/{{maxconn}}/${HAPROXY_MAXCONN}/g" /usr/local/etc/haproxy/haproxy.cfg
+sed -i "s/{{conn_timeout}}/${HAPROXY_CONNECT_TIMEOUT}/g" /usr/local/etc/haproxy/haproxy.cfg
+sed -i "s/{{server_timeout}}/${HAPROXY_SERVER_TIMEOUT}/g" /usr/local/etc/haproxy/haproxy.cfg
+sed -i "s/{{client_timeout}}/${HAPROXY_CLIENT_TIMEOUT}/g" /usr/local/etc/haproxy/haproxy.cfg
 sed -i "s/{{balance}}/${HAPROXY_BALANCE}/g" /usr/local/etc/haproxy/haproxy.cfg
 sed -i "s/auth_key/${REDIS_AUTH_PASSWORD}/g" /usr/local/etc/haproxy/haproxy.cfg
+
 sed -i "s/server master master:6379 check inter 1s/server ${APPNAME}_master ${APPNAME}_master:6379 check inter 1s/g" /usr/local/etc/haproxy/haproxy.cfg
 sed -i "s/server slave slave:6379 check inter 1s/server ${APPNAME}_slave ${APPNAME}_slave:6379 check inter 1s/g" /usr/local/etc/haproxy/haproxy.cfg
 sed -i "s/server slave1 slave1:6379 check inter 1s/server ${APPNAME}_slave1 ${APPNAME}_slave1:6379 check inter 1s/g" /usr/local/etc/haproxy/haproxy.cfg
