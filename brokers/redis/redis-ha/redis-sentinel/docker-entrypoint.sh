00414a4c8e5b62260d129ac54fa22850d8018832
wind634 <2911184332@qq.com>
2017-09-07 15:29:27 +0800

    redis镜像调整

--- brokers/redis/redis-ha/redis-sentinel/docker-entrypoint.sh
+++ brokers/redis/redis-ha/redis-sentinel/docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # sentinel节点的配置方式
-# sentinel节点可以配置多个master,目前只配置一个,*号统一取0
-# REDIS_SENTINEL_MASTER_NAME_* 主机名称
-# REDIS_SENTINEL_MASTER_HOST_*   ip
-# REDIS_SENTINEL_MASTER_PORT_*   port
-# REDIS_SENTINEL_MASTER_QUORUM_*   quorum 默认是2
-# REDIS_SENTINEL_DOWN_AFTER_MILLISECONDS_*  失效时间, 单位是毫秒，默认为30秒
-# REDIS_SENTINEL_FAILOVER_TIMEOUT_*  failover超时时间
-# REDIS_SENTINEL_PARALLEL_SYNCS_* 主备切换时最多可以有多少个slave同时对新的master同步
-# REDIS_SENTINEL_PASSWORD_* 主备切换时最多可以有多少个slave同时对新的master同步
+# sentinel节点可以配置多个master,目前只配置一个
+# REDIS_SENTINEL_MASTER_NAME   master名称
+# REDIS_SENTINEL_MASTER_HOST   host
+# REDIS_SENTINEL_MASTER_PORT   port
+# REDIS_SENTINEL_MASTER_QUORUM   quorum 默认是2
+# REDIS_SENTINEL_DOWN_AFTER_MILLISECONDS  失效时间, 单位是毫秒，默认为30秒
+# REDIS_SENTINEL_FAILOVER_TIMEOUT  failover超时时间
+# REDIS_SENTINEL_PARALLEL_SYNCS 主备切换时最多可以有多少个slave同时对新的master同步
+# REDIS_SENTINEL_PASSWORD 主备切换时最多可以有多少个slave同时对新的master同步
 
 # 配置文件在/usr/local/etc/redis/sentinel.conf 路径下
 
@@ -65,9 +65,6 @@ if [ "$1" = 'redis-sentinel' -a "$(id -u)" = '0' ]; then
                 USE_REDIS_SENTINEL_CONF_FILE="true"
                 redis_sentinel_conf_file_array+=($env_key)
             ;;
-            "REDIS_SENTINEL_MASTER_NAME_"*)
-                master_name_array+=($env_key)
-            ;;
             *)
                 :
             ;;
@@ -122,104 +119,103 @@ if [ "$1" = 'redis-sentinel' -a "$(id -u)" = '0' ]; then
                 exit 1
             fi
 
-            for((i=0; i<len; i++))
-            do
-                file_env ${master_name_array[i]}
-                master_name=${!master_name_array[i]}
+             # master name
+            key='REDIS_SENTINEL_MASTER_NAME'
+            file_env key
+            value=${!key}
+            if [ -n "$value" ];then
+                master_name=$value
+            fi
+            master_name=${master_name:-'mymaster'}
 
-                # 获取mastername对应下标的其他环境变量
-                master_req=${master_name_array[i]:27}
+             # master host
+            key='REDIS_SENTINEL_MASTER_HOST'
+            file_env key
+            value=${!key}
+            if [ -n "$value" ];then
+                master_host=$value
+            fi
+            master_host=${master_host:-'127.0.0.1'}
 
-                 # master ip
-                key='REDIS_SENTINEL_MASTER_HOST_'${master_req}
-                file_env key
-                value=${!key}
-                if [ -n "$value" ];then
-                    master_host=$value
+            # master port
+            key='REDIS_SENTINEL_MASTER_PORT'
+            file_env key
+            value=${!key}
+            if [ -n "$value" ];then
+                if grep '^[[:digit:]]*$' <<< "$value";then
+                    master_port=$value
                 fi
-                master_host=${master_host:-'127.0.0.1'}
+            fi
+            master_port=${master_port:-'6379'}
 
-                # master port
-                key='REDIS_SENTINEL_MASTER_PORT_'${master_req}
-                file_env key
-                value=${!key}
-                if [ -n "$value" ];then
-                    if grep '^[[:digit:]]*$' <<< "$value";then
-                        master_port=$value
+            # master quorum
+            key='REDIS_SENTINEL_MASTER_QUORUM'
+            file_env key
+            value=${!key}
+            if [ -n "$value" ];then
+                if grep '^[[:digit:]]*$' <<< "$value";then
+                    master_quorum=$value
+                fi
+            fi
+            master_quorum=${master_quorum:-'2'}
+
+           echo  "sentinel monitor $master_name $master_host $master_port $master_quorum" >> "$CONFIG"
+
+            # 失效时间
+            key='REDIS_SENTINEL_DOWN_AFTER_MILLISECONDS'
+            file_env key
+            value=${!key}
+            if [ -n "$value" ];then
+                # 大于等于0的数字
+                if grep '^[[:digit:]]*$' <<< "$value";then
+                    if [ $value -gt 0 ]; then
+                        echo  "sentinel down-after-milliseconds $master_name $value" >> "$CONFIG"
                     fi
+                else
+                    echo "sentinel down-after-milliseconds value is not a number."
                 fi
-                master_port=${master_port:-'6379'}
+            fi
 
-                # master quorum
-                key='REDIS_SENTINEL_MASTER_QUORUM_'${master_req}
-                file_env key
-                value=${!key}
-                if [ -n "$value" ];then
-                    if grep '^[[:digit:]]*$' <<< "$value";then
-                        master_quorum=$value
+            # 间隔时间
+            key='REDIS_SENTINEL_FAILOVER_TIMEOUT'
+            file_env key
+            value=${!key}
+            if [ -n "$value" ];then
+                # 大于等于0的数字
+                if grep '^[[:digit:]]*$' <<< "$value";then
+                    if [ $value -gt 0 ]; then
+                        echo  "sentinel failover-timeout $master_name $value" >> "$CONFIG"
                     fi
+                else
+                    echo "sentinel failover-timeout value is not a number."
                 fi
-                master_quorum=${master_quorum:-'2'}
+            fi
 
-               echo  "sentinel monitor $master_name $master_host $master_port $master_quorum" >> "$CONFIG"
-
-                # 失效时间
-                key='REDIS_SENTINEL_DOWN_AFTER_MILLISECONDS_'${master_req}
-                file_env key
-                value=${!key}
-                if [ -n "$value" ];then
-                    # 大于等于0的数字
-                    if grep '^[[:digit:]]*$' <<< "$value";then
-                        if [ $value -gt 0 ]; then
-                            echo  "sentinel down-after-milliseconds $master_name $value" >> "$CONFIG"
-                        fi
-                    else
-                        echo "sentinel down-after-milliseconds value is not a number."
+            # 同步数量
+            key='REDIS_SENTINEL_PARALLEL_SYNCS'
+            file_env key
+            value=${!key}
+            if [ -n "$value" ];then
+                # 大于等于0的数字
+                if grep '^[[:digit:]]*$' <<< "$value";then
+                    if [ $value -gt 0 ]; then
+                        echo  "sentinel parallel-syncs $master_name $value" >> "$CONFIG"
                     fi
+                else
+                    echo "sentinel parallel-syncs value is not a number."
                 fi
+            fi
 
-                # 间隔时间
-                key='REDIS_SENTINEL_FAILOVER_TIMEOUT_'${master_req}
-                file_env key
-                value=${!key}
-                if [ -n "$value" ];then
-                    # 大于等于0的数字
-                    if grep '^[[:digit:]]*$' <<< "$value";then
-                        if [ $value -gt 0 ]; then
-                            echo  "sentinel failover-timeout $master_name $value" >> "$CONFIG"
-                        fi
-                    else
-                        echo "sentinel failover-timeout value is not a number."
-                    fi
-                fi
+            # 认证密码
+            key='REDIS_SENTINEL_PASSWORD'
+            file_env key
+            value=${!key}
+            if [ -n "$value" ];then
+                echo  "sentinel auth-pass $master_name $value" >> "$CONFIG"
+            fi
 
-                # 同步数量
-                key='REDIS_SENTINEL_PARALLEL_SYNCS_'${master_req}
-                file_env key
-                value=${!key}
-                if [ -n "$value" ];then
-                    # 大于等于0的数字
-                    if grep '^[[:digit:]]*$' <<< "$value";then
-                        if [ $value -gt 0 ]; then
-                            echo  "sentinel parallel-syncs $master_name $value" >> "$CONFIG"
-                        fi
-                    else
-                        echo "sentinel parallel-syncs value is not a number."
-                    fi
-                fi
-
-                # 认证密码
-                key='REDIS_SENTINEL_PASSWORD_'${master_req}
-                file_env key
-                value=${!key}
-                if [ -n "$value" ];then
-                    echo  "sentinel auth-pass $master_name $value" >> "$CONFIG"
-                fi
-
-                echo "sentinel config-epoch $master_name  0" >> "$CONFIG"
-                echo "sentinel leader-epoch $master_name  1" >> "$CONFIG"
-
-            done
+            echo "sentinel config-epoch $master_name  0" >> "$CONFIG"
+            echo "sentinel leader-epoch $master_name  1" >> "$CONFIG"
 
         fi
     fi
