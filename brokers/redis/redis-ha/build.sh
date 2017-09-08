698c3ac5db22ba84c02198a4f5db2c4524a795d9
wind634 <2911184332@qq.com>
2017-09-08 14:00:22 +0800

    镜像打上tag

--- brokers/redis/redis-ha/build.sh
+++ brokers/redis/redis-ha/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd .
 cd ./redis-node/
-docker build -t wind634/redis .
+docker build -t wind634/redis:ha .
 cd ..
 cd ./redis-sentinel/
-docker build -t wind634/redis-sentinel  .
+docker build -t wind634/redis-sentinel:ha  .
 cd ..
 cd ./haproxy/
-docker build -t wind634/haproxy-redis  .
+docker build -t wind634/haproxy-redis:ha  .
 
 #docker push wind634/haproxy-redis
 #docker push wind634/redis
