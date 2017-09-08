5a6203965610483aaef7db265be5a25c14cad28c
wind634 <2911184332@qq.com>
2017-09-08 16:33:12 +0800

    镜像打上tag

--- brokers/zookeeper/docker-entrypoint.sh
+++ brokers/zookeeper/docker-entrypoint.sh
@@ -16,10 +16,11 @@
 
 
 # Allow the container to be started with `--user`
-if [ "$1" = 'zkServer.sh' -a "$(id -u)" = '0' ]; then
-    chown -R "$ZOO_USER" "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR"
-    exec su-exec "$ZOO_USER" "$0" "$@"
-fi
+# 以zookeeper用户启动的时候ping会有权限问题, 目前注释
+#if [ "$1" = 'zkServer.sh' -a "$(id -u)" = '0' ]; then
+#    chown -R "$ZOO_USER" "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR"
+#    exec su-exec "$ZOO_USER" "$0" "$@"
+#fi
 
 
 USE_ZOO_CONF_FILE="false"
