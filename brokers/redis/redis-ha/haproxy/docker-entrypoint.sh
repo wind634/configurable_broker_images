169aea738977566a4fc75c86e58fc25f2c7861f4
wind634 <2911184332@qq.com>
2017-09-11 11:56:07 +0800

    镜像打上tag

--- brokers/redis/redis-ha/haproxy/docker-entrypoint.sh
+++ brokers/redis/redis-ha/haproxy/docker-entrypoint.sh
@@ -89,7 +89,7 @@ sed -i "s/server master master:6379 check inter 1s/server ${APPNAME}_master ${AP
 sed -i "s/server slave slave:6379 check inter 1s/server ${APPNAME}_slave ${APPNAME}_slave:6379 check inter 1s/g" /usr/local/etc/haproxy/haproxy.cfg
 sed -i "s/server slave1 slave1:6379 check inter 1s/server ${APPNAME}_slave1 ${APPNAME}_slave1:6379 check inter 1s/g" /usr/local/etc/haproxy/haproxy.cfg
 
-cat /usr/local/etc/haproxy/haproxy.cfg
+#cat /usr/local/etc/haproxy/haproxy.cfg
 
 set -e
 
